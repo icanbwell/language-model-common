335e0663dca9d8aaeed86d519090d01a45c1d772
Imran Qureshi <imranq2@hotmail.com>
2026-03-25 12:56:34 -0700

    Refactor AWS Bedrock client creation to enhance retry configuration and support dynamic region selection

--- languagemodelcommon/aws/aws_client_factory.py
+++ languagemodelcommon/aws/aws_client_factory.py
@@ -1,9 +1,9 @@
 import os
-from typing import Any, cast
+from typing import cast, Literal
 
 import boto3
 from boto3 import Session
-from botocore.config import Config
+from botocore.config import Config, _RetryDict
 
 from types_boto3_bedrock_runtime.client import BedrockRuntimeClient
 from types_boto3_s3.client import S3Client
@@ -34,16 +34,14 @@ class AwsClientFactory:
     # noinspection PyMethodMayBeStatic
     def create_bedrock_client(self) -> BedrockRuntimeClient:
         """Create and return a Bedrock client"""
-        retries_config = cast(
-            Any,
-            {
-                "mode": "adaptive",
-                "max_attempts": self._get_int_env(
-                    name="AWS_BEDROCK_MAX_ATTEMPTS",
-                    default=4,
-                ),
-            },
-        )
+        retries: _RetryDict = {
+            "max_attempts": int(os.getenv("AWS_BEDROCK_MAX_RETRIES", "1")),
+            "mode": cast(
+                Literal["legacy", "standard", "adaptive"],
+                os.getenv("AWS_BEDROCK_RETRY_MODE", "standard"),
+            ),
+        }
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/config.html
         bedrock_config = Config(
             connect_timeout=self._get_float_env(
                 name="AWS_BEDROCK_CONNECT_TIMEOUT_SECONDS",
@@ -53,16 +51,19 @@ class AwsClientFactory:
                 name="AWS_BEDROCK_READ_TIMEOUT_SECONDS",
                 default=180.0,
             ),
-            retries=retries_config,
+            retries=retries,
             tcp_keepalive=True,
         )
+        aws_credentials_profile = os.environ.get("AWS_CREDENTIALS_PROFILE")
+        aws_region_name = os.environ.get("AWS_REGION", "us-east-1")
         session: Session = boto3.Session(
-            profile_name=os.environ.get("AWS_CREDENTIALS_PROFILE")
+            profile_name=aws_credentials_profile,
+            region_name=aws_region_name,
         )
         bedrock_client: BedrockRuntimeClient = session.client(
             service_name="bedrock-runtime",
-            region_name="us-east-1",
             config=bedrock_config,
+            region_name=aws_region_name,
         )
         return bedrock_client
 
